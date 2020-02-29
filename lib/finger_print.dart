a51e5a66063ca9e928bae803bdb916823ec5524f
shahbajjamil <shahbajjamil2014@gmail.com>
2020-02-29 20:08:06 +0530

    fingerPrint function added

---
+++ lib/finger_print.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+
+class FingerPrintAuth extends StatefulWidget {
+  @override
+  _FingerPrintAuthState createState() => _FingerPrintAuthState();
+}
+
+class _FingerPrintAuthState extends State<FingerPrintAuth> {
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+  String _authorizedOrNot = "Not Authorized";
+
+  Future<void> _authorizeNow() async {
+    bool isAuthorized = false;
+    try {
+      isAuthorized = await _localAuthentication.authenticateWithBiometrics(
+        localizedReason: "Verification pending",
+        useErrorDialogs: true,
+        stickyAuth: true,
+      );
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      if (isAuthorized) {
+        _authorizedOrNot = "Authorized";
+        // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyPage()));
+      } else {
+        _authorizedOrNot = "Not Authorized";
+      }
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
